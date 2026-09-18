d95baa8c140cecbbb93dae3ba5c47d8c34c06d74
Sebastian Bejarano <sebz142005@gmail.com>
2026-09-18 11:23:31 -0500

    Inicio de sesion con huella

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     androidTestImplementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
@@ -69,5 +70,6 @@ dependencies {
     implementation("io.ktor:ktor-client-android:3.0.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.mindrot:jbcrypt:0.4")
+    implementation("androidx.biometric:biometric:1.1.0")
 }
 
